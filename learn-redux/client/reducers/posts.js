4f87bef3d4c4ef890f0823dd0dcc5bae53265ed4
Dav Hill <originaldavz@gmail.com>
2016-12-27 21:44:40 +0000

    Step 12

--- learn-redux/client/reducers/posts.js
+++ learn-redux/client/reducers/posts.js
@@ -1,6 +1,15 @@
 function posts (state = [], action) {
-console.log('the post will change');
-    console.log(state, action);
+    switch (action.type) {
+        case 'INCREMENT_LIKES':
+            const index = action.index;
+            return [
+                ...state.slice(0, index),
+                {...state[index], likes: state[index].likes + 1},
+                ...state.slice(index + 1)
+            ]
+        default:
+            return state;
+    }
 
     return state;
 }
