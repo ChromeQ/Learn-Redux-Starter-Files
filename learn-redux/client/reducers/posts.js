e9318a6800ae916b0f4550d686423d2bd39550ee
Dav Hill <originaldavz@gmail.com>
2016-12-28 23:38:39 +0000

    Completed tutorial

--- learn-redux/client/reducers/posts.js
+++ learn-redux/client/reducers/posts.js
@@ -2,6 +2,7 @@ function posts (state = [], action) {
     switch (action.type) {
         case 'INCREMENT_LIKES':
             const index = action.index;
+            
             return [
                 ...state.slice(0, index),
                 {...state[index], likes: state[index].likes + 1},
